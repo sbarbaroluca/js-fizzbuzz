1e63df7c211c2149676dd1817555b39ff7a188f0
lucasbarbaro <sbarbaroluca99@gmail.com>
2021-04-29 14:54:19 +0200

    inizio commenti

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Scrivo un programma che stampi 100 numeri
+
+// Per i numeri che sono sia multipli di 3 che di 5 stampo FizzBuzz
+
+// Per i multipli di 3 stampo Fizz al posto del numero
+
+// Per i multipli di 5 stampo Buzz al posto del numero
+
+
+
